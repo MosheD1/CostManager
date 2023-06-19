3b17b45ad9664a1c09d21c393673813448e20490
moshed1 <moshedego@gmail.com>
2023-06-19 20:19:58 +0300

    connect to mongo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // Moshe Dego 315044511
 // Peleg Vadbeker 209485838
 // Eden Blau 208571927
+const mongoose = require("mongoose");
 const express = require("express");
 const addCostRoute = require('./routes/addCost.js');
 const getReportRoute = require('./routes/report.js');
 const aboutRoute = require('./routes/about.js');
 
 const app = express();
+// Connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://mongo:Aa123456@cluster0.qaa5sqy.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+  });
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
